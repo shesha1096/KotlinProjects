d5f56aebd633dc1503efe35ae51e54a19778b203
Shesha Shankar <shesha1096@gmail.com>
2021-02-07 20:28:48 +1100

    Working on Profile UI

---
+++ CMSApp/app/src/main/java/com/shesha/projects/cmsapp/ui/ProfileUIActivity.kt
@@ -0,0 +1,12 @@
+package com.shesha.projects.cmsapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.shesha.projects.cmsapp.R
+
+class ProfileUIActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_u_i)
+    }
+}
