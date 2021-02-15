b50587d0dea599440f472c25d90f0f1c368918be
Shesha Shankar <sjan0009@student.monash.edu>
2021-02-15 18:25:51 +1100

    Adding RTL support

--- CMSApp/app/src/main/java/com/shesha/projects/cmsapp/ui/LanguagePreferenceActivity.kt
+++ CMSApp/app/src/main/java/com/shesha/projects/cmsapp/ui/LanguagePreferenceActivity.kt
@@ -1,6 +1,7 @@
 package com.shesha.projects.cmsapp.ui
 
 import android.app.AlertDialog
+import android.content.Context
 import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -13,15 +14,42 @@ import com.shesha.projects.cmsapp.utils.LocaleHelper
 class LanguagePreferenceActivity : AppCompatActivity() {
     private lateinit var button : Button
     private lateinit var textView : TextView
+    private var languageCode : String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_language_preference)
         button = findViewById(R.id.select_language_btn)
         textView = findViewById(R.id.display_text_id)
+        val sharedPref = this?.getPreferences(Context.MODE_PRIVATE) ?: return
+        val language = sharedPref.getString(getString(R.string.file_name),"none")
+        if (language != "none")
+        {
+            when (language)
+            {
+                "en" -> {
+                    val languageContext = this?.let { LocaleHelper.setLocale(this,"en") }
+                    val resources = languageContext?.resources
+                    textView.text = resources.getString(R.string.display_message)
+                    window.decorView.layoutDirection = View.LAYOUT_DIRECTION_LTR
+                }
+                "hi" -> {
+                    val languageContext = this?.let { LocaleHelper.setLocale(this,"hi") }
+                    val resources = languageContext?.resources
+                    textView.text = resources.getString(R.string.display_message)
+                    window.decorView.layoutDirection = View.LAYOUT_DIRECTION_LTR
+                }
+                "ar" -> {
+                    val languageContext = this?.let { LocaleHelper.setLocale(this,"ar") }
+                    val resources = languageContext?.resources
+                    textView.text = resources.getString(R.string.display_message)
+                    window.decorView.layoutDirection = View.LAYOUT_DIRECTION_RTL
+                }
+            }
+        }
         button.setOnClickListener(View.OnClickListener {
             var alertDialogBuilder : AlertDialog.Builder = AlertDialog.Builder(this)
             alertDialogBuilder.setTitle("Select Language")
-            val checkedLanguageArray = booleanArrayOf(true,false)
+            val checkedLanguageArray = booleanArrayOf(true,false,false)
             alertDialogBuilder.setMultiChoiceItems(R.array.languageoptions, checkedLanguageArray) { dialog, which, isChecked ->
                 val checkedLanguagesArray = this?.resources?.getStringArray(R.array.languageoptions)
                 if (isChecked)
@@ -33,14 +61,36 @@ class LanguagePreferenceActivity : AppCompatActivity() {
                         val languageContext = this?.let { LocaleHelper.setLocale(this,"en") }
                         val resources = languageContext?.resources
                         textView.text = resources.getString(R.string.display_message)
+                        window.decorView.layoutDirection = View.LAYOUT_DIRECTION_LTR
+                        languageCode = "en"
 
                     }
-                    else{
+                    else if (which == 1){
                         val languageContext = this?.let { LocaleHelper.setLocale(this,"hi") }
                         val resources = languageContext?.resources
                         textView.text = resources.getString(R.string.display_message)
+                        window.decorView.layoutDirection = View.LAYOUT_DIRECTION_LTR
+                        languageCode = "hi"
+                    }
+                    else
+                    {
+                        val languageContext = this?.let { LocaleHelper.setLocale(this,"ar") }
+                        val resources = languageContext?.resources
+                        textView.text = resources.getString(R.string.display_message)
+                        window.decorView.layoutDirection = View.LAYOUT_DIRECTION_RTL
+                        languageCode = "ar"
                     }
                 }
+
+                val sharedPref = this?.getPreferences(Context.MODE_PRIVATE) ?: null
+                if (sharedPref != null) {
+                    with (sharedPref.edit()) {
+                        putString(getString(R.string.file_name), languageCode)
+                        apply()
+                    }
+                }
+
+
 
 
                 // Get the current focused item
@@ -58,6 +108,7 @@ class LanguagePreferenceActivity : AppCompatActivity() {
                 dialog.dismiss()
             })
             alertDialogBuilder.create().show()
+
         })
     }
 }
